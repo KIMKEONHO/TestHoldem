713b1e296c55a5f8584b57c3d646bcef7eea8f18
KIMKEONHO <106578297+KIMKEONHO@users.noreply.github.com>
2026-02-20 10:19:43 +0900

    Add explicit Spring Security core/web dependencies

---
+++ server/src/main/java/com/holdup/server/auth/dto/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.holdup.server.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+    private String displayName;
+}
